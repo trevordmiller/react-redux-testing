c2d0577c69eb14bb4e30e2b6747a6e45c42875a5
Trevor D. Miller <trevordmiller@icloud.com>
2015-10-16 23:27:16 -0600

    Converting components to stateless function components where possible

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import {COLOR_FILL, container, cushion, subtle, alignRight, rounded} from '../shared/styleGuide';
 
-class Quote extends React.Component {
-  render() {
-    const quote = this.props.quote;
-    return (
-      <div
-        style={{
-          ...container,
-          ...cushion,
-          ...rounded,
-          background: COLOR_FILL
-        }}
-        key={quote.id}
-      >
-        <div style={cushion}>
-          "{quote.text}"
-        </div>
-        <div style={{...subtle, ...alignRight}}>
-          {quote.author}
-        </div>
-        <div style={{...subtle, ...alignRight}}>
-          {quote.likeCount} likes
-        </div>
+const Quote = (props) => {
+  const quote = props.quote;
+  return (
+    <div
+      style={{
+        ...container,
+        ...cushion,
+        ...rounded,
+        background: COLOR_FILL
+      }}
+      key={quote.id}
+    >
+      <div style={cushion}>
+        "{quote.text}"
       </div>
-    );
-  }
-}
+      <div style={{...subtle, ...alignRight}}>
+        {quote.author}
+      </div>
+      <div style={{...subtle, ...alignRight}}>
+        {quote.likeCount} likes
+      </div>
+    </div>
+  );
+};
 
 Quote.propTypes = {
   quote: React.PropTypes.object.isRequired
--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -2,26 +2,24 @@ import React from 'react';
 import Quote from './Quote';
 import {container} from '../shared/styleGuide';
 
-class Quotes extends React.Component {
-  render() {
-    const quotes = this.props.quotes.map(quote => {
-      return (
-        <Quote
-          key={quote.id}
-          quote={quote}
-        />
-      );
-    });
+const QuoteList = (props) => {
+  const quotes = props.quotes.map(quote => {
     return (
-      <div style={container}>
-        {quotes}
-      </div>
+      <Quote
+        key={quote.id}
+        quote={quote}
+      />
     );
-  }
-}
+  });
+  return (
+    <div style={container}>
+      {quotes}
+    </div>
+  );
+};
 
-Quotes.propTypes = {
+QuoteList.propTypes = {
   quotes: React.PropTypes.array.isRequired
 };
 
-export default Quotes;
+export default QuoteList;
